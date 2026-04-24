77fd23d7bc37c587a23f9eaa82e13252529563d3
ozgunozyigit <ozgunozyigit@gmail.com>
2026-04-25 01:39:37 +0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Eczane Sipariş Sistemi",
+  description: "Sipariş öneri uygulaması",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="tr">
+      <body>{children}</body>
+    </html>
+  );
+}
